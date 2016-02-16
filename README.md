d18c09719701b4f8c2bbf70b6252f2afa67913e4
Julian Weber <jweber@avarteq.de>
2016-02-16 10:55:10 +0100

    refactors some common methods into new lib file, adds deployment_property_analyzer

--- README.md
+++ README.md
@@ -15,3 +15,16 @@ This script will output job and persistent disk information for the selected dep
 # or for details about job templates
 ./deployment_analyzer <full_path_to_manifest_yml> --details
 ```
+
+## deployment_property_analyzer
+
+This script will output properties for jobs for the selected deployment.
+
+### Usage
+
+```
+./deployment_property_analyzer <full_path_to_manifest_yml> # prints out all properties
+
+# or for details about job templates
+./deployment_property_analyzer <full_path_to_manifest_yml> <job_name> # prints out properties for the given job
+```
---
+++ lib/deployment_file_reader.rb
@@ -0,0 +1,26 @@
+require 'yaml'
+require 'pp'
+require 'erb'
+require 'pp'
+
+def load_yml(file_path)
+  manifest_raw = File.read file_path
+  erb = ERB.new manifest_raw
+  YAML.load erb.result
+end
+
+def get_disk_pools(manifest_hash)
+  manifest_hash['disk_pools']
+end
+
+def get_disk_pool(manifest_hash, disk_pool_name)
+  get_disk_pools(manifest_hash).select { |dp| dp['name'] == disk_pool_name }.first
+end
+
+def get_jobs_with_instances(manifest_hash)
+  manifest_hash['jobs'].select {|job| job['instances'] > 0 }
+end
+
+def get_properties(manifest_hash)
+  manifest_hash['properties']
+end
