9ff29a5f42da52e883e3d6e27ef0e98a706ae546
Julian Weber <jweber@avarteq.de>
2016-02-23 15:46:32 +0100

    adds vm pool analysis

--- lib/deployment_file_reader.rb
+++ lib/deployment_file_reader.rb
@@ -17,6 +17,10 @@ def get_disk_pool(manifest_hash, disk_pool_name)
   get_disk_pools(manifest_hash).select { |dp| dp['name'] == disk_pool_name }.first
 end
 
+def get_resource_pools(manifest_hash)
+  manifest_hash['resource_pools']
+end
+
 def get_jobs_with_instances(manifest_hash)
   manifest_hash['jobs'].select {|job| job['instances'] > 0 }
 end
